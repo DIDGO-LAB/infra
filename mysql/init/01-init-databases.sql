d74937d4a6f6f62140fe4e53b4e90e4541d6dc52
byeok99 <tkdqur3203@gmail.com>
2026-04-30 17:43:07 +0900

    chore: align runtime topology with gateway and mysql

--- mysql/init/01-init-databases.sql
+++ mysql/init/01-init-databases.sql
@@ -6,16 +6,4 @@ CREATE DATABASE IF NOT EXISTS training_db
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE IF NOT EXISTS report_db
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-CREATE USER IF NOT EXISTS 'user_app'@'%' IDENTIFIED BY 'userpassword';
-CREATE USER IF NOT EXISTS 'training_app'@'%' IDENTIFIED BY 'trainingpassword';
-CREATE USER IF NOT EXISTS 'report_app'@'%' IDENTIFIED BY 'reportpassword';
-
-GRANT ALL PRIVILEGES ON user_db.* TO 'user_app'@'%';
-GRANT ALL PRIVILEGES ON training_db.* TO 'training_app'@'%';
-GRANT ALL PRIVILEGES ON report_db.* TO 'report_app'@'%';
-
 FLUSH PRIVILEGES;
